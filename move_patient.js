c2619a4106c19096afbb954d51b75c7f03a89f92
watanabe-tsubasa <t.watanabe423@gmail.com>
2022-10-15 21:25:25 +0900

    watanabe

--- move_patient.js
+++ move_patient.js
@@ -18,7 +18,7 @@ const move_patient = async (current_state, next = "") => {
             return parseInt(element.temp_id,10);
         });
         // console.log(id_list);
-        target_id = id_list.indexOf(Math.min(...id_list));
+        const target_id = id_list.indexOf(Math.min(...id_list));
         // console.log(Math.min(...id_list));
         console.log(target_id);
         store.edit('reception', {
@@ -31,7 +31,7 @@ const move_patient = async (current_state, next = "") => {
     });
     store.edit('reception', {
         search: {
-            next: current_state
+            temp_id: target_id
         }, set: {
             next: next,
             update_time: now.format('HH/mm')
